dfa5d18cde2b21b93545f2b2effe7d2a9a9ad315
unsky559 <6kostya@gmail.com>
2020-03-26 23:50:25 +0200

    Change app.py to pyBackup.py

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # pyBackup
 This python script is intended for automatically creating simple cli zip/rar backup system
 
+## Using
+
+    python pyBackup.py ./import_folder_path ./output_path
+
+To get help use
+
+    python pyBackup.py -help
+
+
 ## Dependencies
 For script is required to use zipfile module. Actually can work out from box, because it is a default module.
 
--- pyBackup.py
+++ pyBackup.py
@@ -2,9 +2,11 @@ import os
 import sys
 import zipfile
 
-
+MAIN_FILE_NAME = "pyBackup.py"
 import_path = sys.argv[1]
 output_path = sys.argv[2]
+archive_name = "backup.zip"
+output_full_path = output_path + "/" + archive_name
 arg = sys.argv
 
 
@@ -16,8 +18,8 @@ def checkFlags(arg = sys.argv):
     if("-help" in arg or "--help" in arg or "-h" in arg):
         print('\n\n')
         print('pyBackup help \n')
-        print('usage: app.py [path to import] [path to export] ')
-        print('example: app.py ./import_folder_path ./output/archive_name \n')
+        print('usage: '+ MAIN_FILE_NAME +' [path to import] [path to export] ')
+        print('example: '+ MAIN_FILE_NAME +' ./import_folder_path ./output_path \n')
         print('-config \t make new settings \n')
         return True
     return False
@@ -27,13 +29,14 @@ if __name__ == "__main__":
     checkFlags()
 
     if(os.access(output_path, os.W_OK)):
-        print('Start backup from ' + import_path + ' to ' + output_path + "/backup.zip")
-        z = zipfile.ZipFile(output_path + "/backup.zip", 'w')
+        print('Start backup from ' + import_path + ' to ' + output_full_path)
+
+        z = zipfile.ZipFile(output_full_path, 'w')
         for root, dirs, files in os.walk(import_path):
             for file in files:
                z.write(os.path.join(root,file))
-
         z.close()
+
         print('Done!')
     else:
         print('Error: can not access to output path')
