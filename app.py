f77d474820f9f0f9a0def8a8a2d914fb3e5a61da
unsky559 <6kostya@gmail.com>
2020-03-26 23:42:22 +0200

    Added os acess check

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import zipfile
 
 
 import_path = sys.argv[1]
-output_path = sys.argv[2] + ".zip"
+output_path = sys.argv[2]
 arg = sys.argv
 
 
@@ -19,19 +19,21 @@ def checkFlags(arg = sys.argv):
         print('usage: app.py [path to import] [path to export] ')
         print('example: app.py ./import_folder_path ./output/archive_name \n')
         print('-config \t make new settings \n')
-
         return True
-
     return False
 
 if __name__ == "__main__":
 
     checkFlags()
-    print('Start backup from ' + import_path + ' to ' + output_path)
-    z = zipfile.ZipFile(output_path, 'w')
-    for root, dirs, files in os.walk(import_path):
-        for file in files:
-           z.write(os.path.join(root,file))
 
-    z.close()
-    print('Done!')
+    if(os.access(output_path, os.W_OK)):
+        print('Start backup from ' + import_path + ' to ' + output_path + "/backup.zip")
+        z = zipfile.ZipFile(output_path + "/backup.zip", 'w')
+        for root, dirs, files in os.walk(import_path):
+            for file in files:
+               z.write(os.path.join(root,file))
+
+        z.close()
+        print('Done!')
+    else:
+        print('Error: can not access to output path')
